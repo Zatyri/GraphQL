f82e5bfc090c9ac681b26b444fbb12ad1f391a25
Oskar <gustosk@gmail.com>
2020-10-02 10:00:49 +0300

    tehtävä 8.4

--- library/index.js
+++ library/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks: [Books]!
+      allBooks(author: String): [Books]!
       allAuthors: [Authors]
   }
 `
@@ -111,7 +111,15 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books,
+      allBooks: (root, args) => {
+          console.log(args);
+          
+          if(!args.hasOwnProperty('author')){
+              return books
+        }
+        
+        return books.filter(book => book.author === args.author)
+    },
       allAuthors: () => authors
   },
   Authors: {
