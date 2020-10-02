e0dbae51d66e734f5c306a2961b2be9e612134d9
Oskar <gustosk@gmail.com>
2020-10-02 09:05:31 +0300

    Tehtävä 8.2
    added nodemon and query allBooks

--- library/index.js
+++ library/index.js
@@ -84,16 +84,26 @@ let books = [
 ]
 
 const typeDefs = gql`
+    type Books {
+        title: String!
+        published: Int!
+        author: String!
+        id: ID!
+        genres: [String!]!
+    }
+
   type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks: [Books!]!
   }
 `
 
 const resolvers = {
   Query: {
       bookCount: () => books.length,
-      authorCount: () => authors.length
+      authorCount: () => authors.length,
+      allBooks: () => books
   }
 }
 
