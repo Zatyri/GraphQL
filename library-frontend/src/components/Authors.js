13087f97ae106d3b891468fcd42d71fd7e78e926
Oskar <gustosk@gmail.com>
2020-10-02 20:08:39 +0300

    Tehtävä 8.8
    added frontend app

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,50 @@
+  
+import React from 'react'
+import {ALL_AUTHORS} from '../queries'
+import {useQuery} from '@apollo/client'
+
+const Authors = (props) => {
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  let authors = []
+
+  if(result.loading){
+    return <div>Loading...</div>
+  }
+
+  authors = result.data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
